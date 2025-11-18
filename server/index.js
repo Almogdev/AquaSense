489c8d9cf075f18ac99a08458383c05d9cc31978
almog <almogluzon@gmail.com>
2025-11-18 18:46:27 +0200

    Add seq-queue and sqlstring modules with initial tests and documentation
    - Introduced seq-queue module for managing ordered task execution.
    - Added comprehensive tests for seq-queue functionality including task management, timeout handling, and error events.
    - Integrated sqlstring module for safe SQL query formatting and escaping.
    - Included detailed README, LICENSE, and HISTORY files for sqlstring.
    - Established package.json files for both modules with necessary dependencies and scripts.

--- server/index.js
+++ server/index.js
@@ -1,3 +1,4 @@
+// libs: express, dotenv, mysql2, cors, morgan.
 const express = require('express');
 const app = express();
 app.use(express.json());
@@ -6,7 +7,6 @@ app.get('/', (req, res) => {
   res.send('Welcome to the Express server!');
 });
 
-// הגדרת פורט (אם אין ב-env, ברירת מחדל 3000)
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
