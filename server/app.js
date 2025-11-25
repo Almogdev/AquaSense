2bf25d3beb2e8d7f101b0fb957632d8d96aae4a4
almog <almogluzon@gmail.com>
2025-11-25 17:56:51 +0200

    Refactor Express server setup and add initial ESP code file

--- server/app.js
+++ server/app.js
@@ -1,13 +1,18 @@
 // libs: express, dotenv, mysql2, cors, morgan.
+require('dotenv').config();
 const express = require('express');
-const app = express();
-app.use(express.json());
-
-app.get('/', (req, res) => {
-  res.send('Welcome to the Express server!');
-});
-
+const morgan = require('morgan');
+const cors = require('cors');
+const path = require('path');
+const routes = require('./routes');
 const PORT = process.env.PORT || 3000;
+const app = express();
+
+app.use(express.static(path.join(__dirname, '../public'))); // Serve static files from the 'public' directory so you don't have to specify 'public' in the URL
+app.use(express.json());
+app.use(morgan('dev'));
+app.use(cors());
+app.use("/", routes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
